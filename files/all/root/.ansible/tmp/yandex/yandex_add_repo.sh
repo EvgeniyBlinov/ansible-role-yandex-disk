848d0f743832f7a0a92ec87b61e0cc45a44aa5cc
Blinov Evgeniy <evgeniy_blinov@mail.ru>
2018-05-18 20:48:16 +0300

    master
    Work stage 2018-05-18 20:48:16
     M defaults/main.yml
     D files/all/root/.ansible/tmp/yandex/yandex_add_repo.sh
     D files/all/root/.ansible/tmp/yandex/yandex_install.sh
     M tasks/main.yml
     M templates/config.cfg
     M templates/iid
     M templates/passwd
    A  templates/yandex-disk.service

--- files/all/root/.ansible/tmp/yandex/yandex_add_repo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "deb http://repo.yandex.ru/yandex-disk/deb/ stable main" |
-    tee -a /etc/apt/sources.list.d/yandex.list > /dev/null &&
-    wget http://repo.yandex.ru/yandex-disk/YANDEX-DISK-KEY.GPG -O- |
-    apt-key add - &&
-    apt-get update
